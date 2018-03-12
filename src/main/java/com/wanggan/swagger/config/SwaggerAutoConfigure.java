3047d4267ec5c3394525258f426c5a4d88963a15
xiangyue520 <1556909500@qq.com>
2018-03-12 16:42:34 +0800

    上传项目

---
+++ src/main/java/com/wanggan/swagger/config/SwaggerAutoConfigure.java
@@ -0,0 +1,71 @@
+package com.wanggan.swagger.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Controller;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.ApiSelectorBuilder;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * @author wanggan
+ */
+@Configuration
+@EnableSwagger2
+@ConditionalOnClass({Controller.class})
+@ConditionalOnMissingBean({Docket.class})
+@EnableConfigurationProperties({SwaggerProperties.class})
+public class SwaggerAutoConfigure {
+    
+    @Autowired
+    SwaggerProperties properties;
+    
+    @Bean
+    public Docket autoEnableSwagger() {
+        String basePackage = properties.getBasePackage();
+        String groupName = properties.getGroupName();
+        Docket docket = new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo());
+       
+        //判断是否存在groupname
+        if(null != groupName && !groupName.trim().equals("")){
+            docket = docket.groupName(groupName);
+        }
+        
+        ApiSelectorBuilder builder = docket.select();
+       
+        //判断是否存在基础扫描包
+        if(null == basePackage || basePackage.trim().equals("")){
+            return builder.build();
+        }else{
+            return builder.apis(RequestHandlerSelectors.basePackage(basePackage))
+                    .build();
+        }
+        
+    }
+    
+    private ApiInfo apiInfo() {
+        Contact contact = new Contact(this.properties.getContactName(), this.properties.getContactUrl(), this.properties.getContactEmail());
+    
+        return new ApiInfoBuilder()
+                .title(this.properties.getTitle())
+                .description(this.properties.getDescription())
+                .termsOfServiceUrl(properties.getTermsOfService())
+                .license(properties.getLicenseName())
+                .licenseUrl(properties.getLicenseUrl())
+                .contact(contact)
+                .version(this.properties.getVersion())
+                .build();
+    }
+}
---
+++ src/main/java/com/wanggan/swagger/config/SwaggerProperties.java
@@ -0,0 +1,109 @@
+package com.wanggan.swagger.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * @author wanggan
+ */
+@ConfigurationProperties("swagger")
+public class SwaggerProperties {
+    private String title = "Swagger Restful API";
+    private String description = "This is a swagger api desc";
+    private String version = "1.0.0";
+    private String termsOfService = "";
+    private String contactName = "";
+    private String contactUrl = "";
+    private String contactEmail = "";
+    private String licenseName = "";
+    private String licenseUrl = "";
+    private String groupName;
+    private String basePackage;
+    
+    public String getTitle() {
+        return title;
+    }
+    
+    public String getDescription() {
+        return description;
+    }
+    
+    public String getTermsOfService() {
+        return termsOfService;
+    }
+    
+    public String getContactName() {
+        return contactName;
+    }
+    
+    public String getContactUrl() {
+        return contactUrl;
+    }
+    
+    public String getContactEmail() {
+        return contactEmail;
+    }
+    
+    public String getLicenseName() {
+        return licenseName;
+    }
+    
+    public String getLicenseUrl() {
+        return licenseUrl;
+    }
+    
+    public String getVersion() {
+        return version;
+    }
+    
+    public String getBasePackage() {
+        return basePackage;
+    }
+    
+    public String getGroupName() {
+        return groupName;
+    }
+    
+    public void setGroupName(String groupName) {
+        this.groupName = groupName;
+    }
+    
+    public void setBasePackage(String basePackage) {
+        this.basePackage = basePackage;
+    }
+    
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    
+    public void setTermsOfService(String termsOfService) {
+        this.termsOfService = termsOfService;
+    }
+    
+    public void setContactName(String contactName) {
+        this.contactName = contactName;
+    }
+    
+    public void setContactUrl(String contactUrl) {
+        this.contactUrl = contactUrl;
+    }
+    
+    public void setContactEmail(String contactEmail) {
+        this.contactEmail = contactEmail;
+    }
+    
+    public void setLicenseName(String licenseName) {
+        this.licenseName = licenseName;
+    }
+    
+    public void setLicenseUrl(String licenseUrl) {
+        this.licenseUrl = licenseUrl;
+    }
+    
+    public void setVersion(String version) {
+        this.version = version;
+    }
+}
